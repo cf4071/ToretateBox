4e7a9592ce5f031593c6d693da49b01f024b5b4c
cf4071 <weiyifu67@gmail.com>
2026-04-24 13:03:47 +0900

    エラーメッセージ調整

--- ToretateBox/src/main/java/katachi/example/toretatebox/controller/GlobalExceptionHandler.java
+++ ToretateBox/src/main/java/katachi/example/toretatebox/controller/GlobalExceptionHandler.java
@@ -10,10 +10,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public String handleException(Exception e, Model model) {
 
-        // ログ確認用（開発中）
         e.printStackTrace();
 
-        // エラーメッセージ
         model.addAttribute("errorMessage",
                 "申し訳ございません。エラーが発生しました。時間をおいて再度お試しください。");
 
